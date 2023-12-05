9a862f826591d4923dd230596b41e2f52d3cb6ab
FernandoENF <fernandoengh@gmail.com>
2023-12-05 19:11:28 -0300

    Add Portuguese language option in locales to date-fns

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -2,6 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import en from "date-fns/locale/en-GB";
 import pl from "date-fns/locale/pl";
+import ptBR from "date-fns/locale/pt-BR";
 import setDefaultOptions from "date-fns/setDefaultOptions";
 
 type LanguageObject = {
@@ -12,6 +13,17 @@ type LanguageObject = {
 type UseLanguageReturnType = {
   languageObject: LanguageObject;
   handleLanguageChange: (language: LanguageObject) => void;
+};
+
+const getDefaultLocale = (lang: string): Locale => {
+  switch (lang) {
+    case "ptBR":
+      return ptBR;
+    case "pl":
+      return pl;
+    default:
+      return en;
+  }
 };
 
 const getLangFromLocalStorage = (): LanguageObject => {
@@ -32,7 +44,7 @@ export const useLanguage = (): UseLanguageReturnType => {
   useEffect(() => {
     const currentLangObject = getLangFromLocalStorage().value;
     i18n.changeLanguage(currentLangObject);
-    setDefaultOptions({ locale: currentLangObject === "pl" ? pl : en });
+    setDefaultOptions({ locale: getDefaultLocale(currentLangObject) });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -44,7 +56,7 @@ export const useLanguage = (): UseLanguageReturnType => {
     (language: LanguageObject) => {
       i18n.changeLanguage(language.value);
       setDefaultLanguage(language);
-      setDefaultOptions({ locale: language.value === "pl" ? pl : en });
+      setDefaultOptions({ locale: getDefaultLocale(language.value) });
     },
     [i18n]
   );
